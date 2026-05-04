f7097759d71937b009f0578296a79bf11e4a1869
Wokrlie <hehelilibz@gmail.com>
2026-05-04 12:13:44 +0800

    feat(camera2d): add camera2d processing logic with a tiny ground demo.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ pub struct GamePlugin;
 #[derive(Component)]
 struct Player;
 
+#[derive(Component)]
+struct MainCamera;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
@@ -14,6 +17,7 @@ fn main() {
 
 fn setup(mut commands: Commands) {
     commands.spawn((
+            MainCamera,
             Camera2d,
             Bloom::NATURAL,
     ));
@@ -24,6 +28,12 @@ fn setup(mut commands: Commands) {
         Srgba{ red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0}, 
         Vec2{ x: 50.0, y: 50.0})
     ));
+
+    // Ground
+    commands.spawn(Sprite::from_color(
+        Srgba{ red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0 }, 
+        Vec2{ x: 150.0, y: 30.0})
+    );
 }
 
 fn update_player(
@@ -45,9 +55,21 @@ fn update_player(
     }
 }
 
+fn camera_follow(
+    player_query: Query<&Transform, With<Player>>,
+    mut camera_query: Query<&mut Transform, (With<Camera>, With<MainCamera>, Without<Player>)>,
+) {
+    let Ok(player_transform) = player_query.single() else { return };
+    let Ok(mut camera_transform) = camera_query.single_mut() else { return };
+
+    camera_transform.translation.x = player_transform.translation.x;
+    camera_transform.translation.y = player_transform.translation.y;
+    camera_transform.translation.z = player_transform.translation.z + 999.0;
+}
+
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup);
-        app.add_systems(Update, update_player);
+        app.add_systems(Update, (update_player, camera_follow));
     }
 }
