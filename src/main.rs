a5d6c330e63207401082f232244fdebc2529a3d6
Wokrlie <hehelilibz@gmail.com>
2026-05-04 11:57:36 +0800

    feat(input): add player update function

--- src/main.rs
+++ src/main.rs
@@ -22,11 +22,28 @@ fn setup(mut commands: Commands) {
     commands.spawn((Player,
         Sprite::from_color(
         Srgba{ red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0}, 
-        Vec2{ x: 10.0, y:10.0})
+        Vec2{ x: 50.0, y: 50.0})
     ));
 }
 
-fn update_player() {}
+fn update_player(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+    mut query: Query<&mut Transform, With<Player>>
+) {
+    // Input
+    const SPEED: f32 = 400.0;
+    for mut transform in query.iter_mut() {
+        let mut direction = Vec3::ZERO;
+        if keyboard_input.pressed(KeyCode::KeyA) { direction.x -= 1.0; }
+        if keyboard_input.pressed(KeyCode::KeyD) { direction.x += 1.0; }
+        if keyboard_input.pressed(KeyCode::KeyW) { direction.y += 1.0; }
+        if keyboard_input.pressed(KeyCode::KeyS) { direction.y -= 1.0; }
+
+        // CHANGED: `Transform`
+        transform.translation += direction * SPEED * time.delta_secs();
+    }
+}
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
